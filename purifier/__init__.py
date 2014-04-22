e1b12aaddf6c99204ba3800770d50f31141ee5c3
PixxxeL <ivan.n.sergeev@gmail.com>
2014-04-22 15:50:15 +0500

    up version

--- purifier/__init__.py
+++ purifier/__init__.py
@@ -13,5 +13,5 @@ Here is purifier.models.PurifyedCharField, purifier.models.PurifyedTextField
 for Django ORM and purifier.forms.PurifyedCharField for Django forms
 """
 
-VERSION = (0, 1, 5,)
+VERSION = (0, 1, 6,)
 __version__ = '.'.join(map(str, VERSION))
